3dded70377166ee57c0893f32cc564fbe56a9c01
Gaurav Yadav <shany.gaurav@gmail.com>
2024-08-24 09:54:33 +0530

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <Routes>
+    <Route path="*" element={<App />} />
+  </Routes>
+  </BrowserRouter>
   </StrictMode>,
 )
